6ee954365e285420fe96378c7cef443e1610fa6f
Shinobu KINJO <kinjo@sensetime.com>
2020-10-29 12:36:16 +0900

    modified:   conditional_batch_normalization.py 	new file:   conditional_instance_normalization.py

--- conditional_instance_normalization.py
+++ conditional_instance_normalization.py
@@ -1,15 +1,19 @@
 from torch import nn
 
-# Modulating early visual processing by language
-# https://arxiv.org/abs/1707.00683
-class ConditionalBatchNormalization(nn.Module):
-    def __init__(self, num_features, num_classes, bias = False, affine = False):
+# A Learned Representation For Artistic Style
+# https://arxiv.org/abs/1610.07629v5
+class ConditionalInstanceNormalization(nn.Module):
+    def __init__(self, num_features, num_classes, bias = False, affine = False,
+            track_running_stats = False):
         super().__init__()
         self.num_features = num_features
         self.num_classes = num_classes
         self.bias = bias
         self.affine = affine
-        self.bn = nn.BatchNorm2d(self.num_features, affine = self.affine)
+        self.track_running_stats = track_running_stats
+        self.instance_norm = nn.InstanceNorm2d(self.num_features,
+                affine = self.affine,
+                track_running_stats = self.track_running_stats)
         if self.bias:
             self.embed = nn.Embedding(self.num_classes, self.num_features * 2)
             self.embed.weight.data[:, : self.num_features].uniform_()
@@ -19,11 +23,11 @@ class ConditionalBatchNormalization(nn.Module):
             self.embed.weight.data.uniform_()
 
     def forward(self, x, y):
-        x = self.bn(x)
+        x = self.instance_norm(x)
         if self.bias:
-            gamma, beta = self.embed(y).chunk(2, 1)
-            z = gamma.view(-1, self.num_features, 1, 1) * x + beta.view(
-                    -1, self.num_features, 1, 1)
+            gamma, beta = self.embed(y).chunk(2, -1)
+            z = gamma.view(-1, self.num_features, 1, 1) * x + beta.view(-1,
+                    self.num_features, 1, 1)
         else:
             gamma = self.embed(y)
             z = gamma.view(-1, self.num_features, 1, 1) * x
